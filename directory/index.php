a72959b5d499d9953d70f46dd5be5bc6ed57fb9d
marleysudbury <marleysudbury@gmail.com>
2016-05-22 21:50:28 +0100

    Directory

---
+++ directory/index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>directory - marleysudbury.github.io</title>
+</head>
+
+<body>
+<?php
+$dir = "../directory";
+
+// Sort in ascending order - this is default
+$a = scandir($dir);
+
+// Sort in descending order
+$b = scandir($dir,1);
+
+print_r($a);
+print_r($b);
+?>
+</body>
+</html>
