751d4e14ecb64c6d709c67565eacc2599bca74a2
marleysudbury <marleysudbury@gmail.com>
2016-05-27 21:25:17 +0100

    css

--- directory/index.php
+++ directory/index.php
@@ -6,17 +6,6 @@
 </head>
 
 <body>
-<?php
-$dir = "../directory";
-
-// Sort in ascending order - this is default
-$a = scandir($dir);
-
-// Sort in descending order
-$b = scandir($dir,1);
-
-print_r($a);
-print_r($b);
-?>
+<?php Print "Hello, World!";?> 
 </body>
 </html>
