ccb7fa284c2f9691c0b4ad249e99ddadf515de0f
marleysudbury <marleysudbury@gmail.com>
2016-05-29 13:04:30 +0100

    form

---
+++ action_page.php
@@ -0,0 +1,25 @@
+
+<!-- saved from url=(0045)http://www.w3schools.com/html/action_page.php -->
+<html><head><meta http-equiv="Content-Type" content="text/html; charset=windows-1252"><title>Forms action page</title>
+</head><body>
+<h2>Your input was received as:</h2>
+<div style="font-size:20px;padding:5px;border:1px solid grey">
+
+Nothing</div>
+
+<p>The server has processed your input and returned this answer.</p>
+<p>Use the back button in the browser to return to the example.</p>
+
+<p>
+<table style="border:1px solid grey"><tbody><tr>
+<th style="width:34px">
+<img src="./action_page_files/lamp.jpg" alt="Note" style="height:32px;width:32px"></th>
+<td>
+The HTML tutorial will not teach you how servers are processing input.
+Server input is explained in our PHP and ASP tutorials.
+</td>
+</tr></tbody></table>
+</p>
+
+
+</body></html>
