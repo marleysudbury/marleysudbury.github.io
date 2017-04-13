dc3dfbd50785091b539bd0b2c882e402e48a1baa
Marley Sudbury <marleysudbury@gmail.com>
2017-04-13 21:18:42 +0100

    Adding program added to cs

--- cs/websiteCS/websiteCS/Program.cs
+++ cs/websiteCS/websiteCS/Program.cs
@@ -6,7 +6,13 @@ namespace websiteCS
 	{
 		public static void Main(string[] args)
 		{
-			Console.WriteLine("Hello World!");
+			Console.WriteLine("ADD TWO NUMBERS TOGETHER");
+			Console.Write("Enter the first number: ");
+			int num1 = int.Parse(Console.ReadLine());
+			Console.Write("Enter the second number: ");
+			int num2 = int.Parse(Console.ReadLine());
+			int sum = num1 + num2;
+			Console.WriteLine("The sum is " + sum);
 		}
 	}
 }
