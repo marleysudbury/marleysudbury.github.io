0fe0192f9dc212138e554b1ef920d226c55cc9a3
marleysudbury <marleysudbury@gmail.com>
2021-06-20 01:28:11 +0100

    Changes made

--- fathers-day/sketch.js
+++ fathers-day/sketch.js
@@ -1,24 +1,28 @@
-var n = 21/53;
+var n = 0;//21/53;
 var c = 53/21;
-let step = 10;
+let step = 1;
 
 function setup(){
-  createCanvas(displayWidth,displayWidth);
+  createCanvas(displayWidth, displayWidth);
   angleMode(DEGREES);
-  colorMode(HSB);
-  // background(0);
+  colorMode(RGB);
+  background(0);
 }
 
 function draw(){
   for (let i = 0; i < step; i++) {
-    var a = n * 137.3;
+    var a = n * (53 + 21);//375.5;
     var r = c * sqrt(n);
     var x = r * cos(a) + width / 2;
     var y = r * sin(a) + width / 2;
-    fill((a*21) % 256, (a*53) % 255, 255);
+    // fill(sin(21 * a) * 255 + (r % 21), cos(53 * a) * 255 + (r%53), sin(53/21 * a) * 255 +(r*cos(a)));
+    // fill(cos(a) * 255, 255 * sin(a), a%255);
+    // let col = map(a%360, 0, 360, 0, 255);
+    let col = map(r, 0, 1440/2, 0, 255)
+    fill(col*21%255,col*53%255,col*21*53%255,255-(step*5));
     noStroke();
     ellipse(x,y,4,4);
     n++;
   }
-  step += 0.1;
+  step *= 1.01;
 }
