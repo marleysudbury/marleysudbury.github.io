44a607ef61b1fd93956c173c3c6bb521511fad82
marleysudbury <marleysudbury@gmail.com>
2025-07-07 18:13:50 +0100

    More work on nested contents list

--- common.js
+++ common.js
@@ -1,5 +1,13 @@
 // Home button function
 function createHomeButton() {
+    // This code puts the "Go home" button on every page
+
+    // The reason that everything is duplicated is that one instance is used to
+    // push the content on the page down by the correct amount whilst the other
+    // sticks to the top of the page for easy access when scrolling.
+
+    // There is probably a more elegant solution.
+
     let node = document.createTextNode("Go home");
     let node2 = document.createTextNode("Go home");
     let anchor = document.createElement("a");
@@ -20,56 +28,94 @@ function createHomeButton() {
 
 // Contents box function
 function createContentsBox() {
+    // This code autogenerates contents boxes for articles
+
     // Check for presence of more than 1 h2 element
     const headings = document.querySelectorAll("h2", "h3", "h4");
     if (headings.length > 0) {
         // Array to track ids used
         const ids = new Array();
+
+        // Int to track list level
+        let listLevel = 0;
+
         // Create contents div and set id for CSS
         const contents = document.createElement("div");
         contents.setAttribute("id", "contents");
+
         // Add title to contents box
         const label = document.createElement("p");
         label.style.textAlign="center";
         label.style.fontWeight="bold";
         label.innerHTML = "Contents";
         contents.appendChild(label);
+
+        // Array for any sublists which need to be created
+        const subLists = new Array();
+
+        // Create contents list
         const contentsList = document.createElement("ol");
+
+        subLists.push(contentsList);
+
+        // Loop through headings and add them to list
         for (let i = 0; i < headings.length; i++) {
+            let newLevel = 0;
             if (headings[i].nodeName == "H2") {
-                let headingId = headings[i].innerText;
-                headingId = headingId.replace(/[^a-z0-9]/gi, '_').toLowerCase();
-                let idNumber = 0;
-                for (let j = 0; j < ids.length; j++) {
-                    if (ids[j] === headingId) {
-                        idNumber++;
-                    }
-                }
-                if (idNumber === 0) {
-                    idNumber = "";
-                } else {
-                    idNumber = "_"+idNumber;
-                }
-                ids.push(headingId);
-                headings[i].setAttribute("id", headingId+idNumber);
-                const a = document.createElement("a");
-                a.setAttribute("href", "#"+headingId+idNumber);
-                a.innerHTML = headings[i].innerText;
-                const item = document.createElement("li");
-                item.appendChild(a);
-                contentsList.appendChild(item);
+                newLevel = 0;
+            } else if (headings[i].nodeName == "H3") {
+                newLevel = 1;
             }
+            
+            // End sublists if present
+            while (listLevel > newLevel) {
+                subLists[subLists.length-2].appendChild(subLists.pop());
+                listLevel--;
+            }
+            // Create sublists if required
+            while (listLevel < newLevel) {
+                const newSubList = document.createElement("ol");
+                listLevel++;
+            }
+
+            let headingId = headings[i].innerText;
+            headingId = headingId.replace(/[^a-z0-9]/gi, '_').toLowerCase();
+            let idNumber = 0;
+            for (let j = 0; j < ids.length; j++) {
+                if (ids[j] === headingId) {
+                    idNumber++;
+                }
+            }
+            if (idNumber === 0) {
+                idNumber = "";
+            } else {
+                idNumber = "_"+idNumber;
+            }
+            ids.push(headingId);
+            headings[i].setAttribute("id", headingId+idNumber);
+            const a = document.createElement("a");
+            a.setAttribute("href", "#"+headingId+idNumber);
+            a.innerHTML = headings[i].innerText;
+            const item = document.createElement("li");
+            item.appendChild(a);
+            contentsList.appendChild(item);
         }
+        while (listLevel > 0) {
+            subLists[subLists.length-2].appendChild(subLists.pop());
+            listLevel--;
+        }
+        // Add list to contents box
         contents.appendChild(contentsList);
+        
+        // Insert contents box before first h2 element
         document.getElementById("bodywrap").insertBefore(contents, headings[0]);
     }
 }
 
-// This code puts the "Go home" button on every page except the home page
+// Check that page is not home page
 if (window.location.pathname != "/") {
+    // Once the DOM content is loaded, run the constructor functions
     window.addEventListener("DOMContentLoaded", createHomeButton);
     window.addEventListener("DOMContentLoaded", createContentsBox);
 }
 
-// This code autogenerates contents boxes for articles
-// Check that page is not home page
